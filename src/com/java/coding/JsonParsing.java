e2ffc6977ad0a76fe610c1d91760ab4985e5a060
ketaki <ketaki>
2016-12-13 22:06:32 -0800

    JsonParsing
    code alignment

--- src/com/java/coding/JsonParsing.java
+++ src/com/java/coding/JsonParsing.java
@@ -20,45 +20,45 @@ public class JsonParsing {
 		String inputJsonStr = "{\"itemList\": {\"items\": [{\"id\": \"item1\"},  " +
 						"{\"id\": \"item2\",\"label\": \"Item 2\"}, " +
 						"{\"id\": \"item3\"}, {\"id\": \"item4\"}, {\"id\": \"item5\"}, " +
-							"{\"id\": \"subItem1\", \"subItems\": [ " +
-							"{\"id\": \"subItem1Item1\",\"label\": \"SubItem 1\"}, " +
-								"{\"id\": \"subItem1Item2\",\"label\": \"SubItem 2\"}, " +
-								"{\"id\": \"subItem1Item3\",\"label\": \"SubItem 3\"} ]}, " +
-									"{\"id\": \"item6\"}, "+
-									"{\"id\": \"item7\",\"label\": \"Item 7\"}, "+
-										"{\"id\": \"subItem2\", " +
-										"\"subItems\": {\"id\": \"item1\",\"label\": \"SubItem 2 item1\"} }]}} ";
+						"{\"id\": \"subItem1\", \"subItems\": [ " +
+						"{\"id\": \"subItem1Item1\",\"label\": \"SubItem 1\"}, " +
+						"{\"id\": \"subItem1Item2\",\"label\": \"SubItem 2\"}, " +
+						"{\"id\": \"subItem1Item3\",\"label\": \"SubItem 3\"} ]}, " +
+						"{\"id\": \"item6\"}, "+
+						"{\"id\": \"item7\",\"label\": \"Item 7\"}, "+
+						"{\"id\": \"subItem2\", " +
+						"\"subItems\": {\"id\": \"item1\",\"label\": \"SubItem 2 item1\"} }]}} ";
 		
 		JSONObject jsonObj = new JSONObject(inputJsonStr);    
 		parseJsonObject("", jsonObj);
 	}
 	
-	 private static void parseJsonObject(String parent, JSONObject jsonObj) {
-		    Iterator<String> it = jsonObj.keys();
-		    while (it.hasNext()) {
-		      String key = (String)it.next();
-		      Object childNode = jsonObj.get(key);
-		      String childKey = parent.isEmpty() ? key : parent + "\\" + key;
-		      checkIfJsonArrayOrObject(childKey, childNode);
-		    }
-		  }
+	private static void parseJsonObject(String parent, JSONObject jsonObj) {
+	    Iterator<String> it = jsonObj.keys();
+	    while (it.hasNext()) {
+	      String key = (String)it.next();
+	      Object childNode = jsonObj.get(key);
+	      String childKey = parent.isEmpty() ? key : parent + "\\" + key;
+	      checkIfJsonArrayOrObject(childKey, childNode);
+	    }
+	 }
 
-	  private static void checkIfJsonArrayOrObject(String parent, Object data) {
-	    if (data instanceof JSONObject) {
-	    	parseJsonObject(parent, (JSONObject)data);
-	    } else if (data instanceof JSONArray) {
-	    	parseJsonArray(parent, (JSONArray) data);
-	    } else {
-	    	System.out.println(parent + "\\"  + data.toString());
-	    }    
-	  }	 
+	private static void checkIfJsonArrayOrObject(String parent, Object data) {
+		if (data instanceof JSONObject) {
+			parseJsonObject(parent, (JSONObject)data);
+		} else if (data instanceof JSONArray) {
+			parseJsonArray(parent, (JSONArray) data);
+		} else {
+			System.out.println(parent + "\\"  + data.toString());
+		}    
+	}	 
 
-	  private static void parseJsonArray(String parent, JSONArray json) {
+	private static void parseJsonArray(String parent, JSONArray json) {
 	    for (int i = 0; i < json.length(); i++) {
 	      Object data = json.get(i);
 	      checkIfJsonArrayOrObject(parent, data);
 	    }
-	  }	
+	}	
 }
 
 
